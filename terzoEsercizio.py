c5063301eee39bf23a2b86bf3166c1e43ebffbdc
repl.it user <replituser@example.com>
2021-02-20 21:22:41 +0000

    terzo es finito

--- terzoEsercizio.py
+++ terzoEsercizio.py
@@ -1,45 +1,61 @@
 class CSVFile():
     def __init__(self,nome):
         self.nome=nome
-
-    def get_data(self):
-        values =[]
-<<<<<<< HEAD
-
-        try:
-            file = open(self.nome,'r')
-        except Exception as s:
-            print('file non esistente "{}"'.format(s))
-            import sys
-            sys.exit()
-
-        
-=======
+        if(type(self.nome) != str):
+            raise Exception ('non sono valide non stringhe come nome del file')
     
-        file = open(self.nome,'r')
->>>>>>> origin/main
-        for line in file:
-            element=line.split(',')
-            
-            if element[0] != 'Date':
-                data = element[0]
-                value = element[1]
-<<<<<<< HEAD
+    def get_data(self, start=None, end=None):
+        values =[]
+
+        if (type(start) != int and start!=None):
+                raise Exception ('non sono validi non int come valori di start')
+        else:
+            if (type(end) != int and end!=None):
+                raise Exception ('non sono validi non int come valori di end')
+            else:
+                
+                
+                nome = self.nome
+                nome = nome.strip()
                 try:
-                    values.append(float(value))
+                    file = open(nome,'r')                        
                 except Exception as s:
-                    print('errore sconosciuto causato dal valore "{}"'.format(s))
+                    print('file non esistente "{}"'.format(s))
+                        
+                i=0
+                for line in file:
+                    if start == end == None:
+                        i=i+1
+                        element=line.split(',')
+                                
+                        if element[0] != 'Date':
+                            data = element[0]
+                            value = element[1]
+
+                            try:
+                                values.append(float(value))
+                            except Exception as s:
+                                print('errore sconosciuto causato dalla conversione valore "{}"'.format(s))
+                    elif i>=start and i<=end:
+                        i=i+1
+                        element=line.split(',')
+                                    
+                        if element[0] != 'Date':
+                            data = element[0]
+                            value = element[1]
+
+                            try:
+                                values.append(float(value))
+                            except Exception as s:
+                                print('errore sconosciuto causato dal valore "{}"'.format(s))
+                    else:
+                        i=i+1
                     
+                file.close()
+                return values
 
-        return values
+miofile = CSVFile("shampoo_sales.csv")
 
-miofile = CSVFile('sampoo_sales.csv')
-=======
 
-                values.append(float(value))
-        return values
-
-miofile = CSVFile('shampoo_sales.csv')
->>>>>>> origin/main
-print(miofile.nome)
-print(miofile.get_data())
+print('Nome del file: "{}"'.format(miofile.nome))
+print('Dati contenuti nel file: "{}"'.format(miofile.get_data()))
